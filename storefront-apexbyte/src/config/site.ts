46f5957502610869ee7301a4091f1589e36db091
CoderBluee <akashsahu.blue@gmail.com>
2026-07-08 17:39:48 +0530

    fix: sync main monorepo with updated storefront branding configs

--- storefront-apexbyte/src/config/site.ts
+++ storefront-apexbyte/src/config/site.ts
@@ -1,22 +1,22 @@
 export const siteConfig = {
-  name: "Brand B",
+  name: "ApexByte",
   tagline: "Enterprise Mail Server & Cloud Mailbox Migrations",
   description:
     "Professional mail server migration software for enterprise mailboxes.",
-  url: "https://brandB.com",
-  ogImage: "https://brandB.com/og.png",
-  email: "support@brandB.com",
+  url: "https://apexbyte.com",
+  ogImage: "https://apexbyte.com/og.png",
+  email: "support@apexbyte.com",
   phone: "+1 (800) 765-4321",
   address: "456 Cloud Plaza, Seattle, WA 98101",
   socials: {
-    twitter: "https://twitter.com/brandB",
-    linkedin: "https://linkedin.com/company/brandB",
-    youtube: "https://youtube.com/@brandB",
-    facebook: "https://facebook.com/brandB",
+    twitter: "https://twitter.com/apexbyte",
+    linkedin: "https://linkedin.com/company/apexbyte",
+    youtube: "https://youtube.com/@apexbyte",
+    facebook: "https://facebook.com/apexbyte",
   },
   links: {
     docs: "/help",
-    github: "https://github.com/brandB",
+    github: "https://github.com/apexbyte",
   },
 };
 
